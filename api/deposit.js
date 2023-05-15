e9f85fc57ea6e56bbac0e993708faa56185a8e9e
Mhd Al Khattab <mohammadalkhatab123@gmail.com>
2023-05-15 17:11:35 +0300

    APIs

--- api/deposit.js
+++ api/deposit.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
-    getDipositItems();
+    getAllDepositItems();
 
-    //  Start Get Diposit
-    function getDipositItems() {
+    //  Start Get Deposit
+    function getAllDepositItems() {
         $.ajax({
             url: 'http://127.0.0.1:8000/api/get-all-deposits',
             headers: { "Authorization": "Bearer " + localStorage.getItem('access_token') },
@@ -15,69 +15,122 @@ $(document).ready(function () {
                 console.log('false');
             },
         }).done(function (items) {
-            let output = `
-            `;
+            let output = '';
+            let state = '';
 
             if (items.data.length !== 0) {
 
                 $.each(items.data, function (key, item) {
 
+                    if(item.state == 0) {
+                        state = `
+                            <div class="footer">
+                                <button class="btn btn-danger canceledButton" data-toggle="modal" data-target=".modal3" data-id="${item.id}">Cancel</button>
+                                <button class="btn btn-success acceptButton" data-toggle="modal" data-target=".modal4" data-id="${item.id}">Accept</button>
+                            </div>
+                        `;
+                    } else {
+                        state = '';
+                    }
+
                     output += `
-                    <div class="row" id="admin-deposit">
-                      <div class="card user">
+
+                    <div class="card user">
                         <i class="fa fa-remove"></i>
-                        <img data-enlargable width="100" src="${item.user.photo}" />
-                        <h3 class="text-center">Mhd Khattab</h3>
+                        <img data-enlargable width="100" src="" />
+                        <h3 class="text-center">${item.user.first_name} ${item.user.last_name} </h3>
                         <div class="row">
                             <h5 class="col-xs-4">Amount:</h5>
-                            <h5 class="col-xs-8">${item.user.amount}</h5>
+                            <h5 class="col-xs-8">${item.amount} </h5>
                         </div>
                         <div class="row">
                             <h5 class="col-xs-4">Process Id:</h5>
-                            <h5 class="col-xs-8">${item.user.proccess_id}</h5>
+                            <h5 class="col-xs-8">${item.proccess_id} </h5>
                         </div>
                         <div class="row">
                             <h5 class="col-xs-4">Wallet:</h5>
-                            <h5 class="col-xs-8">${item.user.wallet}</h5>
+                            <h5 class="col-xs-8">${item.wallet} </h5>
                         </div>
                         <div class="row">
                             <h5 class="col-xs-4">Method:</h5>
-                            <h5 class="col-xs-8">${item.user.method}</h5>
+                            <h5 class="col-xs-8">${item.method}</h5>
                         </div>
                         <div class="row">
-                            <h5 class="col-xs-4">Date:</h5>
-                            <h5 class="col-xs-8">12/3/4555</h5>
-                        </div>
-                        <div class="footer">
-                            <button class="btn btn-danger"data-toggle="modal"data-target=".modal3">Cancel</button>
-                            <button class="btn btn-success"data-toggle="modal"data-target=".modal4">Accepte</button>
-                        </div>
+                            <h5 class="col-xs-4">Date</h5>
+                            <h5 class="col-xs-8">${item.created_at.substring(0,10)}</h5>
                         </div>
 
-                        </div>
-            
-                  </div>
+                        ` + state + `
+
+                    </div>
                     `;
 
                 });
+
                 $('#admin-deposit').empty().append(output);
-                console.log("yes");
-
             } else {
-
-                $('.lodding').remove();
-
-                $('.nodata').remove();
-
                 $('#admin-deposit').parent().append(`<div class="nodata">There Is No Data.</div>`);
-                console.log("yes");
-
             }
 
         });
     }
-    //  End Get Diposit
+    //  End Get Deposit
+
+    // On Accept click
+    $('body').on('click', '.acceptButton', function(e){
+        e.preventDefault();
+
+        localStorage.setItem('deposit_card_id', $(this).data('id'))
+    });
+
+    // On Cancele click
+    $('body').on('click', '.canceledButton', function(e){
+        e.preventDefault();
+
+        localStorage.setItem('deposit_card_id', $(this).data('id'))
+    });
 
 
+    // Accept Deposit
+    $("#acceptForm").submit(function (e) {
+        e.preventDefault();
+        
+
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/accept-deposit/" + localStorage.getItem('deposit_card_id'),
+            headers: { "Authorization": "Bearer " + localStorage.getItem('access_token') },
+            type: "POST",
+            data: $("#acceptForm").serialize(),
+            dataType: 'JSON',
+            success: function (data) {
+                localStorage.removeItem('deposit_card_id');
+                location.reload();  
+            },
+            error: function () {
+                console.log("Error");
+            }
+        });
+    });
+
+    // Cancel Deposit
+    $("#canceledFrom").submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/cancel-deposit/" + localStorage.getItem('deposit_card_id'),
+            headers: { "Authorization": "Bearer " + localStorage.getItem('access_token') },
+            type: "POST",
+            data: $("#canceledFrom").serialize(),
+            dataType: 'JSON',
+            success: function (data) {
+                localStorage.removeItem('deposit_card_id');
+                location.reload();  
+
+            },
+            error: function () {
+                console.log("Error");
+            }
+        });
+    });
 
 });
