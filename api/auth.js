ff64155870b31677566178403a01898613cdf60a
Mhd Al Khattab <mohammadalkhatab123@gmail.com>
2023-05-15 17:23:58 +0300

    Fixed Login

--- api/auth.js
+++ api/auth.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         e.preventDefault();
 
         $.ajax({
-            url: "http://127.0.0.1:8000/api/login",
+            url: "http://127.0.0.1:8000/api/admin-login",
             type: "POST",
             data: $("#loginForm").serialize(),
             dataType: 'JSON',
